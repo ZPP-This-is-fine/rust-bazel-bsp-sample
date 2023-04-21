46f5b5c5181241e348b244f0147c629e41189783
Aleksander Tudruj <aleksanderwt@gmail.com>
2023-04-21 23:07:28 +0200

    added proc macro library to project

--- scripts/remove-tomls.sh
+++ scripts/remove-tomls.sh
@@ -5,5 +5,6 @@ pushd $(dirname $0)/.. > /dev/null
 mv Cargo.toml Cargotoml
 mv hello_world/Cargo.toml hello_world/Cargotoml
 mv hello_lib/Cargo.toml hello_lib/Cargotoml
+mv hello_macro/Cargo.toml hello_macro/Cargotoml
 
 popd > /dev/null
