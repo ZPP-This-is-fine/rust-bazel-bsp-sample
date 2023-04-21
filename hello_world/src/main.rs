dc351644167ec44cc57a5c832bb7426b547ffc0f
Aleksander Tudruj <aleksanderwt@gmail.com>
2023-04-21 23:09:13 +0200

    used proc macro in project

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,13 @@
-extern crate hello_lib;
+
+use hello_macro::AnswerFn;
+
+#[derive(AnswerFn)]
+struct VeryCoolName;
 
 fn main() {
     hello_lib::Greeter::x();
     let hello = hello_lib::Greeter::new("Hello");
     hello.greet("world");
+    println!("The answer is: {}", answer());
+    assert_eq!(42, answer());
 }
